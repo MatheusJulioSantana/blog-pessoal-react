f96f71d969817b17517dc07854378b0c5ac11a3d
Matheus <mattzeppelin.mj@gmail.com>
2024-01-31 09:03:34 -0300

    Deploy da aplicação

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "https://blogpessoal-p61d.onrender.com/",
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
 });
 //função de serviço
 export const cadastrarUsuario = async (
